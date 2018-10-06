b39bb89be7be9082da23d3923f43db2061a8eedc
DESKTOP-EM7Q3HR\ปิ่น <vorasit.pin@gmail.com>
2018-10-06 22:46:14 +0700

    no message

--- connectmysql.php
+++ connectmysql.php
@@ -1,9 +1,6 @@
 <?php
 $mysqlconnect = mysql_connect("localhost","root","12345678") or die("Error Connect to Database");
-
 mysql_query("SET character_set_results=utf8");
 mysql_query("SET character_set_client=utf8");
 mysql_query("SET character_set_connection=utf8");
-
-
 ?>
